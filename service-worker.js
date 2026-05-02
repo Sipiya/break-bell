4ce467cf779027c097b94ec46826413d494a7acf
Sipiya <148962570+Sipiya@users.noreply.github.com>
2026-05-02 10:00:15 +0530

    Update service worker cache name and version

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'break-bell-v4';
+const CACHE_NAME = 'clinical-bell-v5';
 const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
 });
